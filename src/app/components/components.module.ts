8221a516353b11681e368b5c4cb029c5f2b2ce01
andrew <s.s.student@khai.edu>
2022-08-05 20:09:15 +0300

    add base style for user-list

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
-  declarations: [UserFormComponent],
-  exports: [UserFormComponent],
+  declarations: [UserFormComponent, ListItemComponent, UserListComponent],
+  exports: [UserFormComponent, ListItemComponent, UserListComponent],
 })
 export class ComponentsModule {}
